7bd9b70eafd22e02594166266d5ae1e07747ad47
allymf <alyssonfacanhamoreira@gmail.com>
2019-08-06 23:26:08 -0300

    Readding functional source files from last project

--- GenerativeMagicTCC/Template/PlaygroundBook/Sources/IntroViewController.swift
+++ GenerativeMagicTCC/Template/PlaygroundBook/Sources/IntroViewController.swift
@@ -8,26 +8,30 @@
 import UIKit
 
 @objc(Book_Sources_IntroViewController)
-public class IntroViewController: UIViewController {
-    
+class IntroViewController: UIViewController {
+
     var ivMe: UIImageView!
     
-    override public func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
+
+        guard let me = (self.view.subviews.filter {$0.restorationIdentifier == "ivMe"}).first as? UIImageView else { fatalError("Carai Burracha") }
+        self.ivMe = me
         
-        self.ivMe = UIImageView(image: UIImage(named: "me"))
         self.ivMe.layer.cornerRadius = self.ivMe.frame.width/2
         
-        self.view.addSubview(ivMe)
-        
-        NSLayoutConstraint.activate([
-            self.ivMe.widthAnchor.constraint(equalToConstant: 200),
-            self.ivMe.heightAnchor.constraint(equalToConstant: 200),
-            self.ivMe.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            self.ivMe.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
-            
-            ])
-        
+        // Do any additional setup after loading the view.
     }
     
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
